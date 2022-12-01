30efec83bb7db2e2c96b10271c888303e82df60b
Maria Dolores Santini Romero <s8romeromariadolores@gmail.com>
2022-11-30 20:14:55 -0600

    Guardar la imagen

--- firebase.js
+++ firebase.js
@@ -1,68 +1,77 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs, onSnapshot, deleteDoc, doc, getDoc, updateDoc, } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
-import { getStorage, uploadBytes, ref, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-storage.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  onSnapshot,
+  deleteDoc,
+  doc,
+  getDoc,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+import { getStorage, ref, uploadBytes,
+        uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-storage.js"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBDUiTYwl_Qv3ldcSbdHWC9aK_Ca8NKZM0",
   authDomain: "fir-javascript-crud-50660.firebaseapp.com",
   projectId: "fir-javascript-crud-50660",
   storageBucket: "fir-javascript-crud-50660.appspot.com",
   messagingSenderId: "391726803591",
-  appId: "1:391726803591:web:3ed898be857b3cfccb6622",
+  appId: "1:391726803591:web:3ed898be857b3cfccb6622"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const db = getFirestore();
+const db = getFirestore()
+
+export const saveTask = (title, description) =>
+  addDoc(collection(db, 'tasks'), { title, description });
+
 const storage = getStorage();
 
+export const getTasks = () => getDocs(collection(db, 'tasks'))
 
-export const saveTask = (title, description, imageUrl) => addDoc(collection(db, 'tasks'), { title, description, imageUrl });
+export const onGetTasks = (callback) => onSnapshot(collection(db, 'tasks'), callback);
 
-export const getTasks = () => getDocs(collection(db, 'tasks'));
+export const deleteTask = id => deleteDoc(doc(db, 'tasks', id));
 
-export const onGetTasks = (callback) =>
-  onSnapshot(collection(db, 'tasks'), callback);
+export const getTask = id => getDoc(doc(db, 'tasks', id))
 
-export const deleteTasks = id => deleteDoc(doc(db, 'tasks', id));
-
-export const getTask = id => getDoc(doc(db, 'tasks', id));
-
-export const updateTasks = (id, newFields) => updateDoc(doc(db, 'tasks', id), newFields);
+export const updateTask = (id, newFields) => updateDoc(doc(db, 'tasks', id), newFields);
 
 export const saveImage = file => {
   console.log(file);
-  const storageRef = ref(storage, `images/${file.name}`);
 
+  const storageRef = ref(storage, `Images/${file.name}`);
+  uploadBytes(storageRef, file).then((snapshot) => {
+    console.log('Uploaded a blob or file!');
+  });
   const uploadTask = uploadBytesResumable(storageRef, file);
 
-  // Register three observers:
-  // 1. 'state_changed' observer, called any time the state changes
-  // 2. Error observer, called on failure
-  // 3. Completion observer, called on successful completion
-  uploadTask.on('state_changed',
-    (snapshot) => {
-      // Observe state change events such as progress, pause, and resume
-      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      document.querySelector('#progress').value = progress;
-    },
-    (error) => {
-      // Handle unsuccessful uploads
-    },
-    () => {
-      // Handle successful uploads on complete
-      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-        //document.querySelector('#progress').value = 'Fin!';
-        document.querySelector('#image').src = downloadURL;
-        console.log('File available at', downloadURL);
-      });
-    }
-  );
-
-}
+  uploadTask.on('state_changed', 
+  (snapshot) => {
+    // Observe state change events such as progress, pause, and resume
+    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    //console.log('La carga está en un ' + progress + '%');
+    document.querySelector('#progress').style.width = `${progress}%`;
+  }, 
+  (error) => {
+    // Handle unsuccessful uploads
+  }, 
+  () => {
+    // Handle successful uploads on complete
+    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+      //document.querySelector('#progress').value = '¡Completado!'
+      document.querySelector('#image').src = downloadURL;
+      console.log('File available at', downloadURL);
+    });
+  }
+);}
--- index.js
+++ index.js
@@ -1,87 +1,112 @@
-import {saveTask, getTasks, onGetTasks, deleteTasks, getTask, updateTasks, saveImage} from "./firebase.js";
+import { saveTask, getTasks, onGetTasks, deleteTask, getTask, updateTask,
+    saveImage } from './firebase.js';
+    
 
-const taskForm = document.getElementById("task-form");
-const taskContainer = document.getElementById("task-container");
+const taskForm = document.getElementById('task-form')
+const tasksContainer = document.getElementById('task-container')
 
 let editStatus = false;
 let id = '';
 
-const uploadFileAction = (e) => {
-    const file = e.target.files[0];
-     if(file.type.includes('image')) {
-        console.log("Si es una imagen");
-        saveImage(file);
-     }
+const saveSubmit = (e) => {
+   e.preventDefault();
+   const title = formTask['task-title'].value;
+   const description = formTask['task-description'].value;
+   const imageUrl = document.querySelector('#image').src;
+
+   if(title.length > 3 && description.length > 3){
+       if(!editStatus){
+           saveTask(title, description, imageUrl);
+       } else {
+           updateTask(idForEdit, {
+               'title': title, 'description': description
+           });
+           editStatus = false;
+           document.querySelector('#btn-task-save').innerText = 'Save';
+       }
+       document.querySelector('#image').setAttribute('src', '');
+       formTask.reset();
+   } else{
+       alert('Debes escribir algo');
+   }
 }
 
-window.addEventListener("DOMContentLoaded", async () => {
-    
-    onGetTasks( (querySnapshot) => {
+const uploadFileAction = (e) => {
+   const file = e.target.files[0];
 
-        taskContainer.innerHTML = '';
-    
-        querySnapshot.forEach((doc) => {
-            const task = doc.data();
-            taskContainer.innerHTML += `
-                <div class="card card-body mt-2 border-primary">
-                    <h3 class="h5"> ${ task.title } </h3>
-                    <p> ${task.description} </p>
-                    ------
-                    <div>
-                        <button class='btn btn-outline-secondary btn-delete' data-id="${doc.id}">Delete</button>
-                        <button class='btn btn-outline-secondary btn-edit' data-id="${doc.id}">Edit</button>
-                    </div>
-                </div>
-            `;
-        });
-        
+   if(file.type.includes('image')){
+       console.log('si es una imagen')
+       saveImage(file);
+   }
+}
 
-    const btnsDelete = taskContainer.querySelectorAll('.btn-delete')
-        btnsDelete.forEach(btn => {
-            btn.addEventListener('click', ({target: {dataset}}) => {
-                deleteTasks(dataset.id)
-            })
-        })
-        const btnsEdit = taskContainer.querySelectorAll('.btn-edit')
-        btnsEdit.forEach((btn) =>{
-            btn.addEventListener('click', async (e) =>{
-                const doc = await getTask(e.target.dataset.id)
-                const task = doc.data()
+window.addEventListener('DOMContentLoaded', async () => {
 
-                taskForm['task-title'].value = task.title
-                taskForm['task-description'].value = task.description
+   onGetTasks((querySnapshot) => {
+       tasksContainer.innerHTML = '';
+       querySnapshot.forEach(doc => {
+           const task = doc.data()
+           tasksContainer.innerHTML += `
+           <div class="card card-body mt-2 border-primary">
+               <h3 class="h5">${task.title}</h3>
+               <p>${task.description}</p>
+               <img src="${task.imageUrl}"/>
+               <div>
+                   <button class="btn btn-primary btn-delete" data-id="${doc.id}">Eliminar</button>
+                   <button class="btn btn-secondary btn-edit" data-id="${doc.id}">Editar</button>
+               </div>
+               </div>
+           `;
 
-                editStatus = true;
-                id = doc.id;
+       });
 
-                taskForm['btn-task-save'].innerText = 'Update'
-            });
-        });
-    });
-    document.querySelector("#file-task").addEventListener('change', uploadFileAction);
-});
+       const btnsDelete = tasksContainer.querySelectorAll('.btn-delete')
+       btnsDelete.forEach(btn => {
+           btn.addEventListener('click', ({ target: { dataset } }) => {
+               deleteTask(dataset.id)
+           })
+       })
+       const btnsEdit = tasksContainer.querySelectorAll('.btn-edit')
+       btnsEdit.forEach(btn => {
+           btn.addEventListener('click', async (e) => {
+               const doc = await getTask(e.target.dataset.id)
+               const task = doc.data();
+
+               taskForm['task-title'].value = task.title
+               taskForm['task-description'].value = task.description
+
+               editStatus = true
+               id = doc.id;
+
+               taskForm['btn-task-save'].innerText = 'Update'
+
+           })
+       })
+   });
+   document.querySelector('#file').addEventListener('change', uploadFileAction);
+})
 
 taskForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const title = taskForm['task-title'].value;
-    const description = taskForm['task-description'].value;
-    const imageUrl = document.querySelector('#image').src;
+   e.preventDefault()
 
-    if(title.length > 3 && description.length > 3){
+   const title = taskForm['task-title'].value;
+   const description = taskForm['task-description'].value;
 
-    if (!editStatus) {
-        saveTask(title, description, imageUrl);
-        document.querySelector('#image').src = '';
-    }else{
-        updateTasks(id, {title: title, description: description});
+   if (title.length > 3 && description.length >3){
 
-        editStatus = false;
-        document.querySelector('#btn-task-save').innerText = 'Save';
-    }
+   if (!editStatus) {
+       saveTask(title, description)
+   } else {
+       updateTask(id, {
+           title: title,
+           description: description
+       });
 
-     taskForm.reset();
-    }else{
-        alert('Debes de escribir algo');
-    }
-
-});
+       editStatus = false;
+       taskForm['btn-task-save'].innerText = 'Save'
+   }
+   taskForm.reset();
+   }else{
+       alert('debes escribir algo')
+   }
+})
